d9c6411a71c4acc34c10aa48755bb12167a53fe2
Josh Lee <joshswl@gmail.com>
2017-02-20 17:12:44 -0600

    cleaned up css

--- app/fill-in-result.js
+++ app/fill-in-result.js
@@ -1,7 +1,7 @@
 export default function fillResults(element, data) {
   element.querySelector('.result-item__title').innerText = data.show_title;
   element.querySelector('.result-item__category').innerText = data.category;
-  element.querySelector('.result-item__rating').innerText = data.rating;
+  element.querySelector('.result-item__rating--number').innerText = data.rating;
   element.querySelector('.result-item__img').src = data.poster;
   element.querySelector('.results__bottom--summary__description').innerText = data.summary;
   element.querySelector('.results__bottom--actors__description').innerText = data.show_cast;
