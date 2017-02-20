e199aafa2780242950fbace842399dca4cf5798f
Josh Lee <joshswl@gmail.com>
2017-02-20 16:42:51 -0600

    query selectors written, working on fetching from Netflix

--- app/index.js
+++ app/index.js
@@ -1,1 +1,12 @@
 import 'whatwg-fetch';
+import 'fill-in-result';
+
+var searchForm = 0;
+var searchInput = 0;
+var searchResult = 0;
+
+fetch(http://netflixroulette.net/api/api.php?title=Attack%20on%20titan)
+  .then((res) => res.json())
+  .then((data) => {
+    fill-in-result
+  });
