5159ceb1a70c8a3208574ff1ac92a27335da64e4
Josh Lee <joshswl@gmail.com>
2017-02-20 17:06:51 -0600

    results working

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 import 'whatwg-fetch';
-import 'fill-in-result';
+import fillResults from './fill-in-result';
 
-var searchForm = 0;
-var searchInput = 0;
-var searchResult = 0;
+// var searchForm = ;
+// var searchInput = ;
+var searchResult = document.querySelector('.results');
 
-fetch(http://netflixroulette.net/api/api.php?title=Attack%20on%20titan)
+fetch(`http://netflixroulette.net/api/api.php?title=Attack%20on%20titan`)
   .then((res) => res.json())
   .then((data) => {
-    fill-in-result
+    fillResults(searchResult, data);
   });
