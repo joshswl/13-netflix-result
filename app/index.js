9fc82d3ee31a8892fa37c31fb4ca4f45d49895cd
Josh Lee <joshswl@gmail.com>
2017-02-20 21:25:27 -0600

    search bar functioning - first draft

--- app/index.js
+++ app/index.js
@@ -1,12 +1,24 @@
 import 'whatwg-fetch';
 import fillResults from './fill-in-result';
 
-// var searchForm = ;
-// var searchInput = ;
+var searchForm = document.querySelector('.planet__submit');
+var searchInput = document.querySelector('.planet__name');
 var searchResult = document.querySelector('.results');
 
-fetch(`http://netflixroulette.net/api/api.php?title=Attack%20on%20titan`)
-  .then((res) => res.json())
-  .then((data) => {
-    fillResults(searchResult, data);
+function searchForTitle (name) {
+  fetch(`http://netflixroulette.net/api/api.php?title=${name}`)
+    .then((res) => res.json())
+    .then((data) => {
+      fillResults(searchResult, data);
+    });
+}
+
+searchForm.addEventListener('click', () => {
+  searchForTitle(searchInput.value);
+})
+
+  // Listen for the searchBtn to be clicked
+  searchBtn.addEventListener('click', () => {
+    // Run "searchForPlanet" with the input value
+    searchForPlanet(searchInput.value);
   });
