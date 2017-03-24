fb6910f95f5849f81ba755f9185a3810a023ef8c
Josh Lee <joshswl@gmail.com>
2017-03-24 16:24:00 -0500

    second draft

--- app/index.js
+++ app/index.js
@@ -1,24 +1,20 @@
 import 'whatwg-fetch';
 import fillResults from './fill-in-result';
 
-var searchForm = document.querySelector('.planet__submit');
-var searchInput = document.querySelector('.planet__name');
-var searchResult = document.querySelector('.results');
+const searchForm = document.querySelector('.planet__submit');
+const searchInput = document.querySelector('.planet__name');
+const searchResult = document.querySelector('.results');
 
-function searchForTitle (name) {
+function searchForTitle(name) {
   fetch(`http://netflixroulette.net/api/api.php?title=${name}`)
-    .then((res) => res.json())
+    .then(res => res.json())
     .then((data) => {
       fillResults(searchResult, data);
     });
 }
 
+searchForTitle('scrubs');
+
 searchForm.addEventListener('click', () => {
   searchForTitle(searchInput.value);
-})
-
-  // Listen for the searchBtn to be clicked
-  searchBtn.addEventListener('click', () => {
-    // Run "searchForPlanet" with the input value
-    searchForPlanet(searchInput.value);
-  });
+});
